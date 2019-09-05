548926b93150e11e01118f28b1d87d25b2eb6131
mcrabtre <commander.crabtree@gmail.com>
2019-09-05 10:31:12 -0600

    initial file dump

---
+++ NodeCode/node_main_shuffle.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 30 13:13:17 2019
+
+@author: hokis
+"""
+import numpy as np
+import pandas as pd
+import socket
+import queue
+import types
+import SVM
+import Read
+import node_client
+import node_server
+'''
+	Main for the nodes. Starts and waits for data from agg. once received it unpacks data and runs the svm.
+	It then sends the w vector and loss functions back to the agg.
+'''
+
+def run():
+    while True:
+
+        # node detection: determine IP address
+        #nodeIP = socket.gethostbyname_ex("raspberrypi.local")#[-1]
+        #os.system('hostname -I > output.txt') #these 6 commands reads ip address from linux OS
+        #ipFile = open('output.txt', 'r')
+        #nIP = ipFile.readline()
+        #nodeIP = nIP.rstrip(' \n')
+        #ipFile.close()
+        #os.remove('output.txt')
+        nodeIP = '192.168.0.106' '''Currently unable to detect own ip do to identical names. Must be changed for each node'''
+
+        print(nodeIP)
+        # listen for info from aggregator.
+        # determine node number (n) and total number of nodes (Num) from agg Xmission
+        # N is list of node
+        # win,tau,k,d,N,aggIP = node_server.server(node_ip)
+        info = node_server.server(nodeIP) 
+        
+        n = info.node_dict[nodeIP] # node number
+        Num = len(info.node_dict) # total number of nodes
+        d = info.d # number data points/node
+        tau = info.tau # number of local iterations
+        k = info.k # global iteration number
+        host = info.host # aggregator IP
+        shuff = info.shuff # Shuffling type: 0=none,1=random,2=roundrobin,3=segShift
+        
+        # these can be pulled from the agg if desired 
+        weight = 1
+        eta = .01
+        lam = 1
+        
+        # pull data_pts for global update
+		# 	Currently pulling the entire dataset for each node but using a part. 
+        w = info.w
+        D = d*Num# Total data points 
+        filepath = r"train.csv"
+        data_pts = pd.read_csv(filepath, skiprows=(k*D), nrows=(D)).values
+
+        # SVM
+        w,fn = SVM.svm(w,data_pts,eta,lam,tau,d,weight,shuff=shuff,N=Num, n=n)
+            
+        # send w to agg
+        node_client.client(w,fn,host)
+        
+run()
---
+++ mcast/encode_manager.py
@@ -0,0 +1,109 @@
+"""
+Created 15AUG2019 by mcrabtre
+"""
+import pandas as pd
+import numpy as np
+import os
+'''
+    **data partitions**
+    This module manages data partitions and subpartitions for encoded shuffling including 5 nodes.
+    Total data size must be some multiple of 20 data pts. The data set is divided into 5 distinct files. 
+    each node has 1 file in addition to 1/4 of each other nodes file to make a total of 2 file sizes
+    per node. This data is stored in a work.csv file in the encode_manager directory, which is routinely 
+    read from and rewritten as data shuffling occurs. 
+    
+    **encoding/decoding**
+    encoding is a bitwise XOR operation on two file partitions, this results in an encoded partition 
+    decoded is a bitwise XOR operation on the encoded partition and one of its original file partitions.
+    This enables the data of both partitions to be contained within the encoded, a partitions data is 
+    recovered using the other partition it was encoded to.
+    
+    **shuffling stages**
+    shuffling takes place in two stages, the encode send stage, and the cleanup stage. The encode send stage 
+    encodes two partitions and sends it, eventually this will be received by two other nodes.  
+'''
+data_size = 20
+node = 0
+itr = 1
+
+
+def create_workspace(nodeNum,filePath,totalDataSize): #creates a headerless workspace file containing working file and partitions
+    if(totalDataSize % 20) != 0: #check that total data size is a multiple of 20
+        if totalDataSize < 20:
+            totalDataSize = 20
+        else:
+            totalDataSize = totalDataSize - (totalDataSize % 20)
+
+    global data_size #assign to global variables for use by other functions
+    data_size = totalDataSize
+    global node
+    node = nodeNum
+    otherNodes = [1,2,3,4,5]
+    otherNodes.remove(nodeNum)
+    fileSize = int(totalDataSize/5)
+    f = pd.read_csv(filePath, dtype='uint8', skiprows=int((nodeNum - 1) * fileSize), nrows=fileSize).values # write working file from traning data into ndarray f
+    for x in otherNodes: #get partitions of other nodes files
+        if x < nodeNum:
+            p = pd.read_csv(filePath, dtype='uint8', skiprows=int(((x-1)*fileSize)+((nodeNum-2)/4)*fileSize), nrows=fileSize/4).values
+        else:
+            p = pd.read_csv(filePath, dtype='uint8', skiprows=int(((x-1)*fileSize)+((nodeNum-1)/4)*fileSize), nrows=fileSize/4).values
+        f = np.concatenate((f, p), axis=0, out=None)
+
+    np.savetxt("work.csv", f, '%i', delimiter=",") #creates, and saves f into work.csv (in same directory as encode_manager.py)
+
+
+def get_work_file(): #returns current training data for the node. (dependant on shuffle iteration)
+    global data_size
+    file_size = data_size/5
+    return pd.read_csv('work.csv', header=None, dtype='uint8', skiprows=0, nrows=file_size).values
+
+
+def cycle5(value): # for ease of iterative counting
+    if value <= 5:
+        return value
+    else:
+        return cycle5(value-5)
+
+
+def encode(part1, part2): #bitwise XOR encode
+    #newPart = np.zeros(shape= part1.shape, dtype= np.uint8)
+    if part1.shape != part2.shape:
+        raise Exception('Error... both partitions must be of equal dimensions')
+    newPart = part1 ^ part2
+    return newPart
+
+
+def decode(key, part): #partitions are decoded in the same way they are encoded
+    new = encode(key, part)
+    return new
+
+
+# misc testing code
+create_workspace(4, 'test.csv', 40)
+print(cycle5(27))
+p1 = pd.read_csv('test.csv', dtype='uint8', skiprows=15, nrows=2).values
+print('p1 is ',p1)
+
+p2 = pd.read_csv('test.csv', dtype='uint8', skiprows=0, nrows=2).values
+print('p2 is ',p2)
+
+enc = encode(p1,p2)
+print('enc is ',enc)
+dec1 = decode(p2,enc)
+print('dec1 is ',dec1)
+print('enc is ',enc)
+print('p1 is ',p1)
+print('p2 is ',p2)
+
+
+dec2 = decode(p1,enc)
+print('dec2 is ',dec2)
+print('enc is ',enc)
+print('p1 is ',p1)
+print('p2 is ',p2)
+
+wf = get_work_file()
+print('wf is ',wf)
+
+print('dec1 = p1 is ', (dec1 == p1))
+print('dec2 = p2 is ', (dec2 == p2))
